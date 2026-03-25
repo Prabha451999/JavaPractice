6d253b1429268d09f437e4cc19e73fcf04c296bb
Prabha451999 <saprabhakaran1999@gmail.com>
2026-03-25 19:48:26 +0530

    Fourth Changed

---
+++ PracticeSession/StringPrograms.java
@@ -0,0 +1,22 @@
+package org.exercise;
+import java.util.*;
+public class StringPrograms
+{
+	public static void main(String[] args)
+	{
+LinkedList<Integer> ll = new LinkedList<>();
+
+ll.add(1);
+ll.add(2);
+ll.add(3);
+l1.add(4);
+
+System.out.println(ll);
+
+LinkedList<Integer> ll1 = new LinkedList<>();
+
+ll.descendingIterator().forEachRemaining(ll1::add);
+
+System.out.println(ll1);
+}
+}
